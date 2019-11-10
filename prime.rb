538d5a370b6fba6a3935719db0cca984789f4921
Alex Lee <hyunho98@gmail.com>
2019-11-10 00:53:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  factors = 0
+  array = (1..num).to_a
+
+  if num < 2
+    return false
+  end
+
+  array.each{|i|
+      factors += 1 if num % i == 0
+      if factors > 2
+        return false
+      end
+  }
+  true
+end
