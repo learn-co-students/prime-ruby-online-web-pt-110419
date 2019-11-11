935b6b0bb07bfba4e1443743f3b110baa7db3174
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 17:06:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  return false if number <= 1
+ (2...(number)).each do |n|
+      return false if number % n == 0
+  end
+    true
+end
+
+# def prime?(number)
+#   array = []
+#   array = (2...(number)).to_a
+#   array.each do |n|
+#     return false if number % n == 0
+#   end
+#     true
+# end
