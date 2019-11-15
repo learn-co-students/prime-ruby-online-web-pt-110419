b7cded69f53ec444e0f4d218e86cadb3a9440200
Romario Gustave <romagustave10@gmail.com>
2019-11-15 13:02:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+prime_flag = TRUE
+  x = 2 
+  while x <= int / 2)
+  if int % x == 0 
+    prime_flag = FALSE
+    break
+  end
+  x += 1 
+else 
+  TRUE
+end 
+end
