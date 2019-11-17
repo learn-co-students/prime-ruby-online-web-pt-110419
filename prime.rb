6feec2e5b2c61ab1865e05c989ac08bfa8589e6f
Antony Sanders <antony.devon92@gmail.com>
2019-11-17 00:19:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+   if num <= 1
+      return false 
+   elsif num <= 3
+      return true
+   else (2..num/2).none? do |i|
+      num % i == 0 
+    end
+  end
+end
