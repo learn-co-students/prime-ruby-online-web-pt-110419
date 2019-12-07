f7906293b6e7b542151c5373224b92cb7dd18043
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-07 00:30:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
+require 'pry'
+require 'prime'
 # Add  code here!
+def prime?(int)
+ 
+  #if int > 0
+    #return true
+  # elsif int < 0
+    
+  #   return true
+  # else
+  #   puts "#{int}, Be sure to account for negative numbers!"
+  # end 
+  if int < 0 || int == 0 || int == 1
+    return false
+  else
+    
+  (2..int-1).to_a.all? {|a| int % a != 0}
+  end
+end
